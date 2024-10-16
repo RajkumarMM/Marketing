b13d0de94e6ed66dc332bdefea2f9a7d601b661a
RajkumarMM <mmraj816@gmail.com>
2024-10-16 17:17:28 +0530

    responsive check

--- src/components/HelpClient.jsx
+++ src/components/HelpClient.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ImageSlider from './ImageSlider'
+import React from 'react';
+import ImageSlider from './ImageSlider';
 
 function HelpClient() {
   return (
     <div className='text-center text-light my-5 vh-100' style={{ backgroundColor: "#000000" }}>
-        <div className='container'>
-       <h6 className="fw-medium pt-4" style={{ color: "#fa880c" }}>HOW WE'VE HELPED OUR CLIENTS SUCCEED</h6>
-<h3 className="fs-3 fw-bold mt-1 px-3">See how we've helped businesses like yours thrive. Discover the positive impact our work can have.</h3>
-</div>
-<ImageSlider />
+      <div className='container'>
+        <h6 className="fw-medium pt-4" style={{ color: "#fa880c" }}>HOW WE'VE HELPED OUR CLIENTS SUCCEED</h6>
+        <h3 className="fs-4 fw-bold mt-1 px-3">See how we've helped businesses like yours thrive. Discover the positive impact our work can have.</h3>
+      </div>
+      <div className="container-fluid mt-4">
+        <ImageSlider />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HelpClient
+export default HelpClient;
--- src/components/WeDoDifferent.jsx
+++ src/components/WeDoDifferent.jsx
@@ -4,30 +4,35 @@ import { NavLink } from 'react-router-dom';
 
 function WeDoDifferent() {
   return (
-    <div className="text-light vh-100">
-    <div className='container'>
-    <div className="row pt-3">
-  <div className="col-md-6 position-relative">
-    <img src={laptopImage} alt="Team collaboration" className="img-fluid w-100 h-100"/>
-  </div>
-  <div className="col-md-6 p-4 d-flex flex-column justify-content-center">
-    <h6 className="fw-medium mb-3" style={{ color: "#fa880c" }}>WHAT WE DO DIFFERENT</h6>
-    <h3 className="fs-3 fw-bold mb-3">Tired of promises that don't deliver?</h3>
-    <p className="mb-3">
-      You're here because you want <span style={{ color: "#fa880c" }}>real results</span> for your business. We understand the challenges of marketing. Maybe your ads aren't performing as expected, your
-      website isn't converting, or you simply lack the time or resources to manage it all.
-    </p>
-    <p className="mb-3">
-      At KanavuLabs, we're here to help. Our team of experts specializes in creating <span style={{ color: "#fa880c" }}>effective marketing strategies</span> and
-      <span style={{ color: "#fa880c" }}>custom software solutions</span> that drive growth.
-    </p>
-    <p className="mb-3">Ready to <span style={{ color: "#fa880c" }}>take your business to the next level?</span></p>
-    <NavLink to="/contact-us"><button className="btn btn-outline-light w-50">Let's talk</button></NavLink>
-  </div>
-</div>
-</div>
-</div>
+    <div className="text-light vh-md-100">
+      <div className='container'>
+        <div className="row pt-3">
+          {/* Image Section */}
+          <div className="col-md-6 position-relative mb-4 mb-md-0">
+            <img src={laptopImage} alt="Team collaboration" className="img-fluid w-100 h-100" />
+          </div>
+
+          {/* Text Section */}
+          <div className="col-md-6 p-4 d-flex flex-column justify-content-center">
+            <h6 className="fw-medium mb-3" style={{ color: "#fa880c" }}>WHAT WE DO DIFFERENT</h6>
+            <h3 className="fs-4 fw-bold mb-3">Tired of promises that don't deliver?</h3>
+            <p className="mb-3">
+              You're here because you want <span style={{ color: "#fa880c" }}>real results</span> for your business. We understand the challenges of marketing. Maybe your ads aren't performing as expected, your
+              website isn't converting, or you simply lack the time or resources to manage it all.
+            </p>
+            <p className="mb-3">
+              At KanavuLabs, we're here to help. Our team of experts specializes in creating <span style={{ color: "#fa880c" }}>effective marketing strategies</span> and
+              <span style={{ color: "#fa880c" }}>custom software solutions</span> that drive growth.
+            </p>
+            <p className="mb-3">Ready to <span style={{ color: "#fa880c" }}>take your business to the next level?</span></p>
+            <NavLink to="/contact-us">
+              <button className="btn btn-outline-light w-50">Let's talk</button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default WeDoDifferent
+export default WeDoDifferent;
