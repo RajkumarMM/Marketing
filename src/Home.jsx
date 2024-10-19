eb18b22db16ab0bcdbce39e2ab3203b35a7a2e66
RajkumarMM <mmraj816@gmail.com>
2024-10-19 11:52:45 +0530

    conditional render in home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Mousewheel } from 'swiper/modules';
 import Hero from './components/Hero';
 import WhatWeDo from './components/WhatWeDo';
-import WeDoDifferent from './components/WeDoDifferent'
+import WeDoDifferent from './components/WeDoDifferent';
 import SkyRocket from './components/SkyRocket';
 import HelpClient from './components/HelpClient';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -16,43 +16,76 @@ import './Home.css';
 import bannerImage from './assets/Banner.png';
 
 export default function App() {
+  const [isSliderVisible, setIsSliderVisible] = useState(true);
+  const minHeight = 600; // Set your desired minimum height in pixels
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerHeight < minHeight) {
+        setIsSliderVisible(false);
+      } else {
+        setIsSliderVisible(true);
+      }
+    };
+
+    // Check initial height
+    handleResize();
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div style={{ height: '100vh' }}>
-      <Swiper
-        direction="vertical"
-        slidesPerView={1}
-        spaceBetween={30}
-        mousewheel={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Mousewheel, Pagination]}
-        className="page-slider"
-      >
-        <SwiperSlide style={{ 
-          backgroundImage: `url(${bannerImage})`, 
-          backgroundSize: 'cover', 
-          backgroundPosition: 'center',
-          height: '100vh' 
-        }} className='page-slide'>
-          <Hero />
-        </SwiperSlide>
-        <SwiperSlide  className='page-slide'>
-          <WhatWeDo />
-        </SwiperSlide>
-        <SwiperSlide  className='page-slide'>
-          <WeDoDifferent />
-        </SwiperSlide>
-        <SwiperSlide  className='page-slide'>
-          <SkyRocket />
-        </SwiperSlide>
-        <SwiperSlide  className='page-slide'>
-          <HelpClient />
-        </SwiperSlide>
-        <SwiperSlide  className='page-slide'>
-          <Footer />
-        </SwiperSlide>
-      </Swiper>
+    <div style={{ height: '100vh' }}> {/* Change from 100dvh to 100vh for consistency */}
+      {isSliderVisible ? (
+        <Swiper
+          direction="vertical"
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Mousewheel, Pagination]}
+          className="page-slider"
+        >
+          <SwiperSlide style={{ 
+            backgroundImage: `url(${bannerImage})`, 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center',
+          }} className='page-slide'>
+            <Hero />
+          </SwiperSlide>
+          <SwiperSlide className='page-slide'>
+            <WhatWeDo />
+          </SwiperSlide>
+          <SwiperSlide className='page-slide'>
+            <WeDoDifferent />
+          </SwiperSlide>
+          <SwiperSlide className='page-slide'>
+            <SkyRocket />
+          </SwiperSlide>
+          <SwiperSlide className='page-slide'>
+            <HelpClient />
+          </SwiperSlide>
+          <SwiperSlide className='page-slide'>
+            <Footer />
+          </SwiperSlide>
+        </Swiper>
+      ) : (
+        <>
+        <Hero />
+        <WhatWeDo />
+        <WeDoDifferent />
+        <SkyRocket />
+        <HelpClient />
+        <Footer />
+        </>
+      )}
     </div>
   );
 }
